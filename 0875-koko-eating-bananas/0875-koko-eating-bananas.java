195dea57d8b50fb51c31b4a65d777c5d8d309473
Ayush Bansal <pk3535745@gmail.com>
2026-03-09 13:06:24 +0530

    Time: 9 ms (62.13%), Space: 47.6 MB (92.97%), Date: 03-09-2026 - LeetCode

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int check(int[]piles, int mid, int h){
+        long totalHrs = 0;
+        for(int i=0; i<piles.length; i++){
+            totalHrs += (piles[i] + mid-1)/mid; 
+        } 
+        return totalHrs <= h ? 1 : 0;
+    }
+
+
+    public int minEatingSpeed(int[] piles, int h) {
+        
+        int low =1;
+        int high= Arrays.stream(piles).max().getAsInt();
+
+        int ans=1;
+
+        while(low<=high){
+            int mid= (low+high)/2;
+
+            if(check(piles, mid, h)==1){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+
+    }
+}
