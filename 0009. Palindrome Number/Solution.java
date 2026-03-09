e368995f49e402432a4d3363d984f080bf94c623
Ayush <pk3535745@gmail.com>
2026-03-09 11:55:28 +0530

    Time: 5ms (78.88%), Memory: 46.14 MB (31.32%) - LeetCode

---
+++ 0009. Palindrome Number/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+  public static boolean isPalindrome(int x){
+    if(x<0) return false;
+    int reversed = 0;
+    int original = x;
+    while(x != 0){
+      reversed = reversed * 10 + (x%10);
+      x/=10;
+    }
+    return reversed==original;
+  }
+}
