1d5a80db14295a4813e14eb0926d71974b6e568d
Ayush Bansal <pk3535745@gmail.com>
2026-03-09 13:12:50 +0530

    Time: N/A (0%), Space: N/A (0%), Date: 03-09-2026 - LeetCode

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        return bubbleSort(nums);
+    }
+
+    public int[] bubbleSort(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr.length - 1 - i; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
