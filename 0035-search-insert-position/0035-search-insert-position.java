73424a1fa46a86f34fcf945f7435b510dd9acf95
Ayush Bansal <pk3535745@gmail.com>
2026-04-04 23:07:51 +0530

    Time: N/A (0%), Space: N/A (0%), Date: 04-04-2026 - LeetCode

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,27 +1,17 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
 
-        int start = 0;
-        int end = nums.length - 1;
-        int mid = 0;
-
-        while (start <= end) {
-
-            mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
                 return mid;
-            }
-
-            else if (nums[mid] < target) {
-                start = mid + 1;
-            }
-
-            else {
-                end = mid - 1;
+            }else if(nums[mid] < target){
+                low = mid+1;
+            }else{
+                high = mid-1;
             }
         }
-
-        return (target > nums[mid]) ? mid + 1 : mid;
+        return low+1;
     }
 }
