dc5772b7eb26c85137f467277394f8186120cee2
Ayush Bansal <pk3535745@gmail.com>
2026-06-18 09:25:37 +0530

    Time: 2 ms (91.95%), Space: 47 MB (99.6%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i] = nums[insertPos];
+                nums[insertPos] = temp;
+                insertPos++;
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
