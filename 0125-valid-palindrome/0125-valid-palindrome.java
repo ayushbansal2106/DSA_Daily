cbc2738ff6f5163a8ae25a3a7a1423143c7c5f5b
Ayush Bansal <pk3535745@gmail.com>
2026-06-20 13:06:42 +0530

    Time: 2 ms (99.21%), Space: 44.2 MB (84.06%) - LeetHub - Time: O(n), Space: O(1)

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -10,14 +10,17 @@ class Solution {
         		left++;
         	} else if(!Character.isLetterOrDigit(s.charAt(right))) {
         		right--;
-        	} else {
-        		if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+        	} else if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
         			return false;
-        		}
-        		left++;
+        	}else{
+                left++;
         		right--;
-        	}
+            }
         }
         return true;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
