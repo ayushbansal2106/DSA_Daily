3e1b04b1ef57c74ffaf3f5e5ae57ffdad01eeb1f
Ayush Bansal <pk3535745@gmail.com>
2026-07-31 17:32:03 +0530

    Time: 2 ms (47.08%), Space: 43.5 MB (53.9%), Date: 07-31-2026 - LeetCode

---
+++ 0067-add-binary/0067-add-binary-07-31-2026-17-32-01.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        StringBuilder ans = new StringBuilder();
+        int carry = 0;
+        while ((i >= 0) || (j >= 0) || (carry > 0)){
+            int digitA = (i < 0) ? 0 : a.charAt(i--) - '0';
+            int digitB = (j < 0) ? 0 : b.charAt(j--) - '0';
+            int total = digitA + digitB + carry;
+            carry = total / 2;
+            ans.append(total % 2);
+        }
+        return ans.reverse().toString();
+    }
+}
